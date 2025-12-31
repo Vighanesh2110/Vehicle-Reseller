2815ab09625982204060dad8b7d884f3c3399aa5
Vighanesh2110 <vighanesh736@gmail.com>
2025-12-31 11:04:11 +0530

    Update cloudinary_config.py

--- cloudinary_config.py
+++ cloudinary_config.py
@@ -2,7 +2,8 @@ import cloudinary
 import streamlit as st
 
 cloudinary.config(
-    cloud_name=st.secrets["CLOUD NAME"],
+    cloud_name=st.secrets["druva8q8o"],
     api_key=st.secrets["969647552977694"],
     api_secret=st.secrets["J_zSuHl3aWgzT-0A1XaMsP4AvycT"]
 )
+
